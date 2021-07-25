ac87fc3f81a028f3995f345f8337a7b90d12499d
aurelius0523 <aurelius0523@gmail.com>
2021-07-26 00:01:08 +0800

    Add kubernetes configuration. Update readme

---
+++ create-images.bat
@@ -0,0 +1,8 @@
+@REM Build service artifact and docker image
+call mvn clean install -DskipTests -f admin-client/pom.xml
+call docker build -t aurelius0523/admin-client-docker:latest admin-client
+call mvn clean install -DskipTests -f admin-server/pom.xml
+call docker build -t aurelius0523/admin-server-docker:latest admin-server
+
+@REM prune untagged docker images. WARNING, it will also tag non-admin service dangling images!
+call echo y|docker image prune
